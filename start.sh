11e4eceb29626ecfe899f2ca03f799a7f46c7bef
sr_hosseini <rassoulhosseini@gmail.com>
2018-11-02 01:22:22 +0330

    bug fixed

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 git clone frontend.bundle
 git clone backend.bundle
-docker run --rm --interactive --tty --volume $PWD/frontend:/app --working-dir /app alexsuch/angular-cli npm install
-docker run --rm --interactive --tty --volume $PWD/backend:/app composer install
+docker run --rm --interactive --tty -v $PWD/frontend:/app -w /app alexsuch/angular-cli npm install
+docker run --rm --interactive --tty -v $PWD/backend:/app composer install
 docker-compose up -d
